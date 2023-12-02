440a19291b765e242741e493e3a70ab03cf5503d
SuneetBansal <32817920+SuneetBansal@users.noreply.github.com>
2023-12-02 13:43:45 +0530

    updating envs

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 console.log('*****************  Here are the env variables ************************');
-console.log(process.env);
+console.log(process.env['INPUT_OPEN-AI-TOKEN']);
+console.log(process.env['INPUT_MODEL-NAME']);
+console.log(process.env['INPUT_PROMPT']);
